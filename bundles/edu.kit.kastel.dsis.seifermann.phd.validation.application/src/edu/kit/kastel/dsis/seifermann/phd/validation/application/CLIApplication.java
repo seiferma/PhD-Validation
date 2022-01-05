a5df7d1117ad37f387862db1158da11211b9b9d8
Stephan Seifermann <stephan.seifermann@kit.edu>
2022-01-05 15:59:55 +0100

    Initial commit of dummy application.

---
+++ bundles/edu.kit.kastel.dsis.seifermann.phd.validation.application/src/edu/kit/kastel/dsis/seifermann/phd/validation/application/CLIApplication.java
@@ -0,0 +1,20 @@
+package edu.kit.kastel.dsis.seifermann.phd.validation.application;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class CLIApplication implements IApplication {
+
+    @Override
+    public Object start(IApplicationContext context) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
